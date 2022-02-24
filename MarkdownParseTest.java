ac0a8559b178854c46083091a9051e11f5bbd421
mBookUCSD <93008597+mBookUCSD@users.noreply.github.com>
2022-02-24 11:47:24 -0800

    Made Stuff for Lab 8

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -187,6 +187,42 @@ public class MarkdownParseTest {
         List<String> expect = List.of("a.com");
         assertEquals(MarkdownParse.getLinks(contents), expect);
     }
+    @Test
+    public void testSnip1() throws IOException, NoSuchFileException {
+
+        ArrayList<String> correctOutput = new ArrayList<>();
+        correctOutput.addAll(Arrays.asList("`google.com","google.com","ucsd.edu"));
+        Path fileName= Path.of("lab8-snip1.md");
+        // read the file contents into a string
+	    String contents = Files.readString(fileName);
+        // run getLinks on the contents of the file
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertEquals(correctOutput,links);
+    }
+    @Test
+    public void testSnip2() throws IOException, NoSuchFileException {
+
+        ArrayList<String> correctOutput = new ArrayList<>();
+        correctOutput.addAll(Arrays.asList("a.com(())","example.com"));
+        Path fileName= Path.of("lab8-snip2.md");
+        // read the file contents into a string
+	    String contents = Files.readString(fileName);
+        // run getLinks on the contents of the file
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertEquals(correctOutput,links);
+    }
+    @Test
+    public void testSnip3() throws IOException, NoSuchFileException {
+
+        ArrayList<String> correctOutput = new ArrayList<>();
+        correctOutput.addAll(Arrays.asList("https://ucsd-cse15l-w22.github.io/"));
+        Path fileName= Path.of("lab8-snip3.md");
+        // read the file contents into a string
+	    String contents = Files.readString(fileName);
+        // run getLinks on the contents of the file
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertEquals(correctOutput,links);
+    }
     
 }
 
---
+++ lab8-snip2.md
@@ -0,0 +1,5 @@
+[a [nested link](a.com)](b.com)
+
+[a nested parenthesized url](a.com(())c )
+
+[some escaped \[ brackets \]](example.com)
